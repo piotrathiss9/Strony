5e2777b8a6f3917368249eb1e903e956b49a154a
piotrathiss9 <82943058+piotrathiss9@users.noreply.github.com>
2021-12-21 12:25:46 +0100

    Add values in database

--- Baza.sql
+++ Baza.sql
@@ -4,3 +4,16 @@ email TEXT NOT NULL,
 pasw TEXT NOT NULL,
 nick TEXT NOT NULL,
 nazwisko TEXT NOT NULL);
+
+INSERT INTO Users
+(name,
+ email,
+ pasw,
+ nick,
+ nazwisko)
+ VALUES
+ ("Adam",
+ "adam@poczta.com",
+ "Haslo123";
+ "Adusz",
+ "Wiercicki")
