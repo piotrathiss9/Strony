89bcc6b26efb25a12b39484959bc7f715bd7944b
piotrathiss9 <82943058+piotrathiss9@users.noreply.github.com>
2021-12-29 13:13:53 +0100

    Update SI.php

--- SI.php
+++ SI.php
@@ -16,8 +16,8 @@ session_start();
  Email: <input type="text" name="Email"></input><br/>
  Hasło: <input type="password" name="Haslo"></input><br><br>
  <input type="submit" value="Zaloguj się"/>
- <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
- <a href="index.html"><img src="ZNAKfirmy.png" /></a>
+ <br><br><br><br><br><br><br><br><br>
+ <a href="index.html"><img src="ZNAKfirmy.png" id="logo"/></a>
 
 </form>
 <?php
